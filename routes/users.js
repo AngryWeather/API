bd38b006403548c7e984eae540f8be3e3afab816
Weronika <WerRic96@protonmail.com>
2023-05-14 16:54:04 +0200

    Rename controller to usersController

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
-const controller = require("../controllers/users_controller");
+const usersController = require("../controllers/users_controller");
 const categoriesController = require("../controllers/categories_controller")
 
 // router.get('/', controller.authenticateToken, controller.getStudents);
-router.post('/register', controller.addUser);
-router.post('/login', controller.login);
+router.post('/register', usersController.addUser);
+router.post('/login', usersController.login);
 
 module.exports = router;
