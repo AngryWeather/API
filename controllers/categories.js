bb935c8e50e1ed0edd6efd19d0cf4b7af8ea1f21
Weronika <WerRic96@protonmail.com>
2023-05-14 16:51:13 +0200

    Export categories

--- controllers/categories.js
+++ controllers/categories.js
@@ -2,3 +2,7 @@ const getCategories = async () => {
     const response = await fetch("https://opentdb.com/api_category.php");
     return await response.json();
 }
+
+module.exports = {
+    getCategories,
+};
