225917c7d484437e67306865709ea4943c649eea
Weronika <WerRic96@protonmail.com>
2023-05-14 16:52:10 +0200

    Rename categories to categories_controller

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const controller = require("../controllers/users_controller");
+const categoriesController = require("../controllers/categories_controller")
 
 // router.get('/', controller.authenticateToken, controller.getStudents);
 router.post('/register', controller.addUser);
