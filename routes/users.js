d8fc5f1f9669c9557b96b7aea42b98dca621214e
Weronika <WerRic96@protonmail.com>
2023-05-21 00:19:18 +0200

    Remove unnecessary code

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 const usersController = require("../controllers/users_controller");
 
-// router.get('/', controller.authenticateToken, controller.getStudents);
 router.post('/register', usersController.addUser);
 router.post('/login', usersController.login);
 
