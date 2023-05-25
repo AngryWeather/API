44ce476a5eba84311a08da5c848319d7bc9c8b6e
Weronika <WerRic96@protonmail.com>
2023-05-26 01:44:50 +0200

    Add localhost to cors

--- app.js
+++ app.js
@@ -1,23 +1,30 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var cors = require("cors");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var triviaRouter = require('./routes/trivia');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var triviaRouter = require("./routes/trivia");
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 
-app.use('/', indexRouter);
-app.use('/user', usersRouter);
-app.use('/trivia', triviaRouter);
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+
+app.use("/", indexRouter);
+app.use("/user", usersRouter);
+app.use("/trivia", triviaRouter);
 
 module.exports = app;
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,16 +1,15 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
-const pool = require('../connection');
+const pool = require("../connection");
 const { json } = require("express");
 
 dotenv.config();
 process.env.TOKEN_SECRET;
 
-const generateAccessToken = email => {
-    return jwt.sign(email, process.env.TOKEN_SECRET, {expiresIn: "120s"});
-}
-
+const generateAccessToken = (email) => {
+  return jwt.sign(email, process.env.TOKEN_SECRET, { expiresIn: "120s" });
+};
 
 // const getStudents = (req, res) => {
 //     pool.query("SELECT * FROM users", (error, results) => {
@@ -19,58 +18,67 @@ const generateAccessToken = email => {
 //     });
 // }
 
-const validateEmail = email => {
-    const regex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
-    return email.match(regex) ? true : false;
-}
+const validateEmail = (email) => {
+  const regex =
+    /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+  return email.match(regex) ? true : false;
+};
 
 const addUser = async (req, res) => {
-    try {
-        let userBool = await userExists(req.body.username, req.body.email);
-        if (userBool === true) {
-            res.send("user exists");
-        } else if (!validateEmail(req.body.email)) {
-            res.status(400).json();
-        } else {
-            const hashedPassword = await bcrypt.hash(req.body.password, 10);
-            const newUser = await pool.query("INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING *",
-            [req.body.username, req.body.email, hashedPassword]);
-            res.json(newUser.rows);
-        }
-        
-    } catch (error) {
-        throw error;
+  try {
+    let userBool = await userExists(req.body.username, req.body.email);
+    if (userBool === true) {
+      res.send("user exists");
+    } else if (!validateEmail(req.body.email)) {
+      res.status(400).json();
+    } else {
+      const hashedPassword = await bcrypt.hash(req.body.password, 10);
+      const newUser = await pool.query(
+        "INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING *",
+        [req.body.username, req.body.email, hashedPassword]
+      );
+      res.json(newUser.rows);
     }
-}
-
-
+  } catch (error) {
+    throw error;
+  }
+};
 
 const login = async (req, res) => {
-    try {
-        const {email, password} = req.body;
-        const users = await pool.query("SELECT * FROM users WHERE email = $1", [email]);
-        
-        if (users.rows.length === 0) return res.status(401).json({error: "Email is incorrect"});
-        
-        const validPassword = await bcrypt.compare(password, users.rows[0].password);
+  try {
+    const { email, password } = req.body;
+    const users = await pool.query("SELECT * FROM users WHERE email = $1", [
+      email,
+    ]);
 
-        if (!validPassword) return res.status(401).json({error: "Incorrect password"});
-        
-        // return res.status(200).json("Success");
-        const token = generateAccessToken({email});
-        return res.json(token);
-    } catch (error) {
-        throw error;
-    }
+    if (users.rows.length === 0)
+      return res.status(401).json({ error: "Email is incorrect" });
 
-}
+    const validPassword = await bcrypt.compare(
+      password,
+      users.rows[0].password
+    );
+
+    if (!validPassword)
+      return res.status(401).json({ error: "Incorrect password" });
+
+    // return res.status(200).json("Success");
+    const token = generateAccessToken({ email });
+    return res.json(token);
+  } catch (error) {
+    throw error;
+  }
+};
 
 const userExists = async (username, email) => {
-    const len = await pool.query("SELECT * FROM users WHERE (username = $1) OR (email = $2)", [username, email]);
-    return len.rows.length > 0;
-}
+  const len = await pool.query(
+    "SELECT * FROM users WHERE (username = $1) OR (email = $2)",
+    [username, email]
+  );
+  return len.rows.length > 0;
+};
 
 module.exports = {
-    addUser,
-    login,
+  addUser,
+  login,
 };
