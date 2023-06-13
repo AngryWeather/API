82cbcd307720502f0af7df97c3a9ba514cbd175c
Weronika <WerRic96@protonmail.com>
2023-06-13 23:34:02 +0200

    Fix code styling

--- controllers/trivia_controller.js
+++ controllers/trivia_controller.js
@@ -1,50 +1,56 @@
 const getCategories = async (req, res) => {
-    const response = await fetch("https://opentdb.com/api_category.php");
-    const json = await response.json();
-    res.status(200).json(json);
-}
+  const response = await fetch("https://opentdb.com/api_category.php");
+  const json = await response.json();
+  res.status(200).json(json);
+};
 
-const isNumOfQuestionsValid = numOfQuestions => {
-    return (numOfQuestions > 0) && (numOfQuestions < 51);
-}
+const isNumOfQuestionsValid = (numOfQuestions) => {
+  return numOfQuestions > 0 && numOfQuestions < 51;
+};
 
 const postTriviaData = (req, res, next) => {
-    const {numOfQuestions} = req.body;
-    
-    if (!isNumOfQuestionsValid(numOfQuestions)) {
-        res.status(400).json({error: "Number of questions must be between 1 and 50 (both inclusive)"});
-    }
+  const { numOfQuestions } = req.body;
 
-    next();
-}
+  if (!isNumOfQuestionsValid(numOfQuestions)) {
+    res.status(400).json({
+      error: "Number of questions must be between 1 and 50 (both inclusive)",
+    });
+  }
+
+  next();
+};
 
 const generateURL = async (req) => {
-    let url = `https://opentdb.com/api.php?amount=${req.body.numOfQuestions}`;
+  let url = `https://opentdb.com/api.php?amount=${req.body.numOfQuestions}`;
 
-    if (req.body.category !== undefined) {
-        const response = await fetch("https://opentdb.com/api_category.php");
-        const categories = await response.json();
-        const id = categories.trivia_categories.find(n => n.name === req.body.category).id;
-        url += `&category=${id}`;        
-    } if (req.body.type !== undefined) {
-        url += `&type=${req.body.type}`;
-    } if (req.body.difficulty !== undefined) {
-        url += `&difficulty=${req.body.difficulty}`;
-    }
+  if (req.body.category !== undefined) {
+    const response = await fetch("https://opentdb.com/api_category.php");
+    const categories = await response.json();
+    const id = categories.trivia_categories.find(
+      (n) => n.name === req.body.category
+    ).id;
+    url += `&category=${id}`;
+  }
+  if (req.body.type !== undefined) {
+    url += `&type=${req.body.type}`;
+  }
+  if (req.body.difficulty !== undefined) {
+    url += `&difficulty=${req.body.difficulty}`;
+  }
 
-    return url;
-}
+  return url;
+};
 
 const generateForm = async (req, res) => {
-    const url = await generateURL(req);
-    const response = await fetch(url); 
-    const trivia = await response.json();
-    
-    res.status(200).json(trivia);
-}
+  const url = await generateURL(req);
+  const response = await fetch(url);
+  const trivia = await response.json();
+
+  res.status(200).json(trivia);
+};
 
 module.exports = {
-    getCategories,
-    postTriviaData,
-    generateForm,
+  getCategories,
+  postTriviaData,
+  generateForm,
 };
